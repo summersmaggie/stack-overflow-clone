457869f4df213e353630160b55b325d03227e68f
Hannah Lee and Maggie Summers <maggie@>
2018-04-24 13:58:29 -0700

    create question views, model, and controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root :to => 'welcome#index'
 
+    resources :questions do
+      resources :answers
+    end
+
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
