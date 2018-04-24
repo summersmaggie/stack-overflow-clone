f97d9f6fbb467d0246a92caf664656dd6934dcf8
Hannah Lee and Maggie Summers <hannah@>
2018-04-24 14:16:30 -0700

    create index, new, show files and methods for questions.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,27 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @questions = Question.all
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:question_title, :question_body)
+    end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
-
+  has_many :answers
+  validates :question_title, :presence => true
+  validates :question_body, :presence => true
 
 end
